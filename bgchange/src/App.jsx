b41b2b4d253663d358129bc3be7b6beb4d889f91
Abhishek-Kr-Pandit <abhishekrpandit18@gmail.com>
2025-02-03 16:56:44 +0530

    bg change project

---
+++ bgchange/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div className="w-full min-h-screen duration-200 absolute top-0 left-0" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+          <button
+          onClick={()=>setColor("red")} //onclick expects a function that's why aise likha
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+          <button
+          onClick={()=>setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>green</button>
+          <button
+          onClick={()=>setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg k" style={{backgroundColor:"blue"}}>blue</button>
+          <button
+          onClick={()=>setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:"yellow"}}>yelllow</button>
+          <button
+          onClick={()=>setColor("black")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"black"}}>black</button>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
